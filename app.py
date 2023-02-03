2929581bbe5acae77498b831cc1903884aee6596
nelsonvelis <nvelis@terpmail.umd.edu>
2023-02-02 19:03:03 -0500

    misspelled first lol

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Nelson D Velis! Im adding my frist code change.'
+    return 'Hello World from Nelson D Velis! Im adding my first code change.'
 
 @app.route('/hello')
 def hello():
